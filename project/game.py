e67763066b9f5baae539106137866f8f8561aa68
Anh Tran Thi Van <vivianttva@gmail.com>
2026-08-28 19:46:42 +0700

    changed in game.py

--- project/game.py
+++ project/game.py
@@ -2,4 +2,4 @@
 #Thi Van Anh Tran
 name=input("What is your name? ")
 age=input("How old are you? ")
-print("Hello " + name + "! You are " + age + " years old.")
+print(f'Hello {name} ! You are {age} years old.')
